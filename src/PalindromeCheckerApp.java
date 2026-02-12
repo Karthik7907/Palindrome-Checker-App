ea2a5ebf2b44f92a29f9c99dbfb3e333b1e1dffd
Karthik7907 <vb7723@srmist.edu.in>
2026-02-12 16:06:43 +0530

    UC1: Application Entry & Welcome Message

---
+++ src/PalindromeCheckerApp.java
@@ -0,0 +1,19 @@
+/*
+USECASE1 Palindrome Checker App
+Descripton:
+This class represents the entry point of the
+Palindrome Management System
+
+At this stage, the application:
+Starts execution from the main
+ */
+
+
+public class PalindromeCheckerApp {
+    public static void main(String[] args){
+        System.out.println("Welcome to the Palindrome Checker Management System");
+        System.out.println("Versin : 1.0");
+        System.out.println("System Initialized Successfully ");
+
+    }
+}
